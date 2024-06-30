5608710d4761f2954bcafd156c489558bb838085
Ambroise Bareux <abareux@student.42mulhouse.fr>
2024-06-30 15:40:27 +0200

    UwU

--- Makefile
+++ Makefile
@@ -1,2 +1,6 @@
 oups:
 	c++ main.cpp -o oups -lX11
+
+re:
+	rm -rf oups
+	c++ main.cpp -o oups -lX11
--- main.cpp
+++ main.cpp
@@ -10,7 +10,7 @@ int main(void) {
 	unsigned long long int time = 0;
 
     while (true) {
-		XWarpPointer(dpy, None, root_window, 0, 0, 0, 0, (time % 4 == 2 || time % 4 == 3) * 100, (time % 4 == 1 || time % 4 == 2) * 100);
+		XWarpPointer(dpy, None, root_window, 0, 0, 0, 0, (time % 4 == 2 || time % 4 == 3) * 100 + 3000, (time % 4 == 1 || time % 4 == 2) * 100 + 1500);
 		XFlush(dpy);
 		//system("xset dmps force off");
 		//system("xinput disable 11");
